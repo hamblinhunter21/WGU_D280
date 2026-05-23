2c60ba4dde970845e90d2287d390b5671e426049
Hunter Hamblin <Hamblinhunter21@gmail.com>
2026-05-23 13:53:59 -0600

    routing wip

--- first-app/src/app/app.routes.ts
+++ first-app/src/app/app.routes.ts
@@ -1,1 +1,14 @@
-export const routes: any[] = [];
+import { Routes } from '@angular/router';
+import { WorldComponent } from './world/world.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'map',
+    pathMatch: 'full'
+  },
+  {
+    path: 'map',
+    component: WorldComponent
+  }
+];
